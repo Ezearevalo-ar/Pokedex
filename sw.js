fe6feff7c80ab95ee0fbafd2f35acc4f2a2b0f0b
Ezequiel Thomas Arevalo <ezearevalodev@gmail.com>
2023-06-09 15:45:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -57,7 +57,7 @@ self.addEventListener("fetch", (e) => {
             return res;
           })
           .catch((error) => {
-            return fetch("https://dwt3bv-pokedex-pwa.netlify.app/error.html")
+            return fetch("/error.html")
               .then((res) => {
                 return res;
               });
